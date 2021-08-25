ef0879f5f9d1a173bb1f6ebe8bd75ee767026027
Lisi Case <49213376+lisicase@users.noreply.github.com>
2021-08-25 00:58:49 -0700

    added use of default values for vocab ulary annotations

--- src/Microsoft.OData.Edm/Vocabularies/Annotations/EdmVocabularyAnnotation.cs
+++ src/Microsoft.OData.Edm/Vocabularies/Annotations/EdmVocabularyAnnotation.cs
@@ -15,6 +15,7 @@ namespace Microsoft.OData.Edm.Vocabularies
         private readonly IEdmTerm term;
         private readonly string qualifier;
         private readonly IEdmExpression value;
+        private readonly bool usesDefault;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EdmVocabularyAnnotation"/> class.
@@ -44,7 +45,54 @@ namespace Microsoft.OData.Edm.Vocabularies
             this.term = term;
             this.qualifier = qualifier;
             this.value = value;
+            this.usesDefault = false;
         }
+
+        /// <summary>
+        /// NEW: Initializes a new instance of the EdmVocabularyAnnotation class
+        /// to the default value.
+        /// </summary>
+        /// <param name=“target”>Element the annotation applies to.</param>
+        /// <param name=“term”>Term bound by the annotation</param>
+        public EdmVocabularyAnnotation(IEdmVocabularyAnnotatable target, IEdmTerm term)
+            //: this(target, term, null, new EdmStringConstant(term.DefaultValue))
+        {
+            // Check arguments
+            if (term.DefaultValue == null) // throw error if no default value
+            {
+                //throw new ArgumentNullException("value");
+                EdmUtil.CheckArgumentNull(new EdmStringConstant(null), "value");
+            }
+            EdmUtil.CheckArgumentNull(target, "target");
+            EdmUtil.CheckArgumentNull(term, "term");
+
+            // Find default value
+            string defaultValue = term.DefaultValue;
+            IEdmExpression defaultValueExp = new EdmStringConstant(defaultValue);
+
+            // Check arguments
+            EdmUtil.CheckArgumentNull(target, "target");
+            EdmUtil.CheckArgumentNull(term, "term");
+
+            // Initialize
+            this.target = target;
+            this.term = term;
+            this.qualifier = null;
+            this.value = defaultValueExp;
+            this.usesDefault = true;
+        }
+
+        /* This constructor is ambiguous with the other three-param constructor...
+        /// <summary>
+        /// NEW: Initializes a new instance of the EdmVocabularyAnnotation class 
+        /// to the default value.
+        /// </summary>
+        /// <param name=“target”>Element the annotation applies to.</param>
+        /// <param name=“term”>Term bound by the annotation</param>
+        /// <param name=“qualifier”>Qualifier used to distinguish bindings.</param>
+        public EdmVocabularyAnnotation(IEdmVocabularyAnnotatable target, IEdmTerm term,
+        string qualifier)
+        { }*/
 
         /// <summary>
         /// Gets the element the annotation applies to.
@@ -76,6 +124,14 @@ namespace Microsoft.OData.Edm.Vocabularies
         public IEdmExpression Value
         {
             get { return this.value; }
+        }
+
+        /// <summary>
+        /// Gets Whether the annotation uses a default value
+        /// (not defined with a provided value).
+        /// </summary>
+        public bool UsesDefault {
+            get { return this.usesDefault; }
         }
     }
 }
